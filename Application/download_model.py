1345865ed19395a129f75b6bcb38af9368f78fa8
patel hetu <Hetu.pict22@sot.pdpu.ac.in>
2024-07-17 19:12:05 +0530

    Update download_model.py

--- Application/download_model.py
+++ Application/download_model.py
@@ -2,7 +2,7 @@ import gdown
 
 # Google Drive file ID and output path
 file_id = 'hGuYthH2MKLZdtWxBfjpQqWNuoBGRIar'
-output = 'saved_model/model.safetensors'
+output = 'fine-tuned-abstractive/model.safetensors'
 
 # Download the file
 gdown.download(f"https://drive.google.com/uc?id={file_id}", output, quiet=False)
